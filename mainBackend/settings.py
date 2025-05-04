75621dc0da733b3629ebf8658ee2a5d5c513a3e9
ndolocr <crndolo@gmail.com>
2025-05-04 15:41:19 +0300

    Creating User Management App

--- mainBackend/settings.py
+++ mainBackend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # Custom Apps
     'dashboard',
+    'user_management',
 ]
 
 MIDDLEWARE = [
@@ -142,3 +143,4 @@ STORAGES = {
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'user_management.User'
---
+++ user_management/models.py
@@ -0,0 +1,74 @@
+from django.db import models
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
+
+class UserModuleManager(BaseUserManager):
+    use_in_migrations = True 
+
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('The Email must be set')
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)  # Properly set the password
+        user.save(using=self._db)  # Ensure the correct database is used
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)        
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        
+        return self._create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    class Meta:
+        db_table = 'user_management'
+
+    password = models.CharField(max_length=255)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)        
+    email = models.EmailField(max_length=100, unique=True)    
+    date_activated = models.DateTimeField(auto_now_add=True)    
+    phone = models.CharField(max_length=255, blank=True, null=True)
+    gender = models.CharField(max_length=10, blank=True, null=True)
+    last_name = models.CharField(max_length=255, blank=True, null=True)
+    first_name = models.CharField(max_length=255, blank=True, null=True)
+    middle_name = models.CharField(max_length=255, blank=True, null=True)
+    country_code = models.CharField(max_length=10, blank=True, null=True)
+
+    updated_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserModuleManager()
+
+class Student(models.Model):    
+    admittedOn = models.DateField(blank=True, null=True)
+    admissionNumber = models.CharField(max_length=255, blank=True, null=True)
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+class Teacher(models.Model):
+    dateJoined = models.DateField(blank=True, null=True)
+    StaffNumber = models.CharField(max_length=255, blank=True, null=True)
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Parent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    children = models.ManyToManyField(Student, related_name='parents')
