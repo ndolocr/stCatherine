737c432bb1616728a64d5062350ce2a50a7bd52e
ndolocr <crndolo@gmail.com>
2025-05-07 22:04:37 +0300

    Loging out

--- user_management/views.py
+++ user_management/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth import logout as django_logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='login')
+def logout(request):
+    django_logout(request)
+    return redirect('login')
