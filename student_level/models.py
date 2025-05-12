2e348d166354348a20a9c3f7fd88aa0bf0d27f48
ndolocr <crndolo@gmail.com>
2025-05-12 21:04:09 +0300

    Creating models for student level - (class) and stream

--- student_level/models.py
+++ student_level/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
+from user_management.models import User
 # Create your models here.
 
-class student_level(models.Model):
+class StudentLevel(models.Model):
     class Meta:
         db_table = 'class'    
 
     updated_on = models.DateTimeField(auto_now=True)
     created_on = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=255, blank=True, null=True)
-    # created_by = models.ForeignKey(Staff, on_delete=models.CASCADE, related_name='created_by')
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_by')
